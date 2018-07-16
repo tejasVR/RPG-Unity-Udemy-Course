b4584735f6d3cf0378ea3a312b15f5567f8eafd5
Tejas Shroff <tejasshroff24@gmail.com>
2018-07-16 12:58:45 -0400

    Adding third person character and smooth fixed camera

---
+++ Assets/Game/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform _playerObj;
+    public float _cameraFollowSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void LateUpdate () {
+
+        transform.position = Vector3.Lerp(transform.position, _playerObj.transform.position, Time.deltaTime * _cameraFollowSpeed);
+		
+	}
+}
